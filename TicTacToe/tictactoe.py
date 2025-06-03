a083b8d1d97d5a90aee3317b7760e638a6ac340e
AndreiChihai <Andrei.Chihai.extern@dvag.com>
2025-06-03 21:50:04 +0300

    Initialized tictactoe and created the board

---
+++ TicTacToe/tictactoe.py
@@ -0,0 +1,28 @@
+print("Welcome to the TicTacToe game!");
+
+userFigureChoice = input('Do you want to be X or O');
+
+s1 = '1';
+s2 = '2';
+s3 = '3';
+s4 = '4';
+s5 = '5';
+s6 = '6';
+s7 = '7';
+s8 = '8';
+s9 = '9';
+
+
+row1 = [s1,s2,s3];
+row2 = [s4,s5,s6];
+row3 = [s7,s8,s9];
+
+def createBoard():
+    print(f"A  {s1} | {s2} | {s3}")
+    print("  ---+---+---")
+    print(f"B  {s4} | {s5} | {s6}")
+    print("  ---+---+---")
+    print(f"C  {s7} | {s8} | {s9}")
+
+createBoard();
+    
